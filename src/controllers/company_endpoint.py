edb52edc93f8ff30a79d9585682314519ddbca3e
leonardo.silva <leonardo.silva@sesatech.com.br>
2024-08-27 16:27:48 -0300

    Paths fix

--- src/controllers/company_endpoint.py
+++ src/controllers/company_endpoint.py
@@ -13,11 +13,3 @@ from config.deps import get_pg_session
 
 router = APIRouter()
 
-@router.get('/', status_code=status.HTTP_201_CREATED, response_model=CompanySchema)
-async def get_companies(db: AsyncSession = Depends(get_pg_session())):
-    async with db as session:
-        query = select(Company)
-        result = await session.execute(query)
-        companies: List[Company] = result.scalars().all()
-
-        return companies
