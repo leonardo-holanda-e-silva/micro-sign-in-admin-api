2e3fb5fbea68b3abf9fbb744ec7edd39dd884ad9
leonardo.silva <leonardo.silva@sesatech.com.br>
2024-09-27 23:22:36 -0300

    .env variables fix

--- config/configs.py
+++ config/configs.py
@@ -1,23 +1,28 @@
 import os
-
-from pydantic import BaseSettings, AnyHttpUrl
-from sqlalchemy.ext.declarative import declarative_base
-from typing import List
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    """
-    General API configurations
-    """
+    # API configuration
     API_V1_STR: str = os.environ.get("API_V1_STR")
+
+    # Postgres database configuration
     DB_POSTGRES_USER: str = os.environ.get("DB_POSTGRES_USER")
     DB_POSTGRES_PASS: str = os.environ.get("DB_POSTGRES_PASS")
     DB_POSTGRES_IP: str = os.environ.get("DB_POSTGRES_IP")
     DB_POSTGRES_PORT: str = os.environ.get("DB_POSTGRES_PORT")
     DB_POSTGRES_BASE: str = os.environ.get("DB_POSTGRES_BASE")
     DB_POSTGRES_URL: str = f"postgresql+asyncpg://{DB_POSTGRES_USER}:{DB_POSTGRES_PASS}@{DB_POSTGRES_IP}:{DB_POSTGRES_PORT}/{DB_POSTGRES_BASE}"
-    RABBIT_MQ_URL: str = os.environ.get("RABBIT_MQ_URL")
+
+    # RabbitMQ configuration
+    RABBIT_MQ_USER: str = os.environ.get("RABBIT_MQ_USER")
+    RABBIT_MQ_PASS: str = os.environ.get("RABBIT_MQ_PASS")
+    RABBIT_MQ_IP: str = os.environ.get("RABBIT_MQ_IP")
+    RABBIT_MQ_PORT: str = os.environ.get("RABBIT_MQ_PORT")
+    RABBIT_MQ_URL: str = f"amqp://{RABBIT_MQ_USER}:{RABBIT_MQ_PASS}@{RABBIT_MQ_IP}:{RABBIT_MQ_PORT}/"
     RABBIT_MQ_REGISTRY_QUEUE: str = os.environ.get("RABBIT_MQ_REGISTRY_QUEUE")
     RABBIT_MQ_REGISTRY_DLQ: str = os.environ.get("RABBIT_MQ_REGISTRY_DLQ")
+
+    # MongoDB configuration
     MONGO_DB_USER: str = os.environ.get("MONGO_DB_USER")
     MONGO_DB_PASS: str = os.environ.get("MONGO_DB_PASS")
     MONGO_DB_IP: str = os.environ.get("MONGO_DB_IP")
@@ -28,4 +33,5 @@ class Settings(BaseSettings):
     class Config:
         case_sensitive = True
 
+# Instantiate the settings
 settings = Settings()
